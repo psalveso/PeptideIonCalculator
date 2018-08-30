33451410eb8ee952d09bcfd1c54e4328bf95109c
Patrick Salveson <patricksalveson@gmail.com>
2018-08-30 15:27:50 -0700

    typo fixes

--- run_PMC.py
+++ run_PMC.py
@@ -1,25 +1,18 @@
 from peptide_mass_calculator import *
 
 
-# Tries to open seuqnece file and build the required objects for script to round
+# Tries to open seuqnece file and build the required objects for script to run
 # Creates a sequence file otherwise
 while True:
     try:
         aaIonMasses = getAminoAcidMasses()
-        print('Loading data from json...')
         sequence = getSequenceAAs()
-        print('Sequence file found, reading now...')
         uniqueAA = getUniqueAA(sequence)
-        print('Getting unique amino acids...')
         mass = getExactMass(sequence, aaIonMasses)
-        print('Calculating mass...')
         possibleMasses = populateMassDictionary(mass, sequence, uniqueAA, aaIonMasses)
-        print('Calculating possible ions...')
         break
     except IOError:
-        print("No sequence file found, creating now...")
         createSequenceFile()
-
 
 # User searches for a match, ensures that the user enters a float
 while True:
